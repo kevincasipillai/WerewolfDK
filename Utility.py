a867a409f7286896e1281136f7f82fd17ca16a03
Kevin <k_casipillai@live.dk>
2020-08-26 23:26:43 +0200

    Locate hidden players and fixed bugs, with wrong players chosen, because index was "i" was i+1 when searching for players.

--- Utility.py
+++ Utility.py
@@ -1,7 +1,5 @@
-# Hej med dig, dette er en utility script
-# test 2
-
 import random
+import numpy as np
 
 def number_of_players_func():
     number_of_players = int(input("Enter Number of players: "))
@@ -40,7 +38,7 @@ def characters(x_players):
                       "Villager",
                       "Villager"]
     number_of_characters = x_players+4                                                      # the number of players + 4 additional characters
-    choose_characters_for_game = []
+    choosen_characters_for_game = []
     character_num = int(input(f"""
 Choose {str(number_of_characters)} different characters. Hit return after each choice!
 
@@ -63,16 +61,36 @@ Choose {str(number_of_characters)} different characters. Hit return after each c
 
 First character: """))
 
-    choose_characters_for_game.append(character_num)
+    while character_num > 16 or character_num < 1:      # Making sure the first charachter is a valid number
+        print(" ")
+        print("Invalid charachter!")
+        character_num = int(input("Choose another charachter: "))
 
-    for i in range(0, number_of_characters-1):
+    choosen_characters_for_game.append(character_num)
+    print(" ")
+
+    for i in range(0, number_of_characters-1):          # Choose the rest of the charachters and making sure the chose charachters are not repeated or invalid (outside 1-16)
         character_num = int(input("Next character: "))
-        choose_characters_for_game.append(character_num)
+        while character_num in choosen_characters_for_game or character_num > 16 or character_num < 1:
+            print(" ")
+            print("Invalid charachter or charachter is already chosen!")
+            character_num = int(input("Choose another charachter: "))
 
-    chosen_characters = [character_list[i] for i in choose_characters_for_game] # characters chosen to play with
+        choosen_characters_for_game.append(character_num)
+        print(" ")
+
+    choosen_characters_for_game.sort()
+    choosen_characters_for_game = [x-1 for x in choosen_characters_for_game]
+    all_chosen_characters = [character_list[i] for i in choosen_characters_for_game] # characters chosen to play with
     random_character = random.sample(range(0, number_of_characters), x_players)
-    characters = [chosen_characters[i] for i in random_character]
-    # hidden_characters = [random_character[i] for i not in chosen_characters] # Find hidden characters
+    playing_characters = [all_chosen_characters[i] for i in random_character]
 
-    return characters, chosen_characters
+    hidden_characters_num = []                                                          # Find hidden charachters with for loop
+    for i in range(0, len(all_chosen_characters)):
+        if i not in random_character:
+            hidden_characters_num.append(i)
 
+    hidden_characters_num.sort()
+    hidden_characters = [all_chosen_characters[i] for i in hidden_characters_num]
+
+    return all_chosen_characters, playing_characters, hidden_characters
