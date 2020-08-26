5b9020d27b9d980e22460a6a0393348b954e82fe
Kevin <k_casipillai@live.dk>
2020-08-26 23:26:43 +0200

    Locate hidden players and fixed bugs, with wrong players chosen, because index was "i" was i+1 when searching for players. Fixed so that it will ask again if wrong or invalid number is entered, when choosing charachters.

--- Main_file.py
+++ Main_file.py
@@ -3,15 +3,15 @@ from Utility import *
 number_of_players = number_of_players_func()                                                # Choose number of players and return the value
 players = player_names(number_of_players)                                                   # Return list with player names
 character_info = characters(number_of_players)
-character = character_info[0]
-all_characters = character_info[1]
-#hidden_characters = character_info[2]
+all_characters = character_info[0]
+character = character_info[1]
+hidden_characters = character_info[2]
 
 
 # HIDDEN INFORMATION IN-GAME
 for i in range(0, number_of_players):
     print(f"{players[i]} is {character[i]}")
 
+print(f"Hidden charachters are {hidden_characters}")
 print(f"All characters are {all_characters}")
 
-# Find out how to just find hidden characters which are the four lying on the table
