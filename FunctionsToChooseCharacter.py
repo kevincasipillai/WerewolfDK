e70166de4a06aa2faad68b9b7e3d3f0a0dc47e01
Kevin <k_casipillai@live.dk>
2020-09-23 17:11:52 +0200

    + Added new window with choosing character + Added new file with function for each character (not fully working yet)

---
+++ FunctionsToChooseCharacter.py
@@ -0,0 +1,116 @@
+# Denne funktion virker ikke helt endnu
+
+
+def f_C1():
+    if C1 == 0:
+        C1 = C1+1
+    elif C1 == 1:
+        C1 = C1-1
+
+    print("this works")
+
+
+def f_C2():
+    if C2 == 0:
+        C2 = C2+1
+    elif C2 == 1:
+        C2 = C2-1
+
+
+def f_C3():
+    if C3 == 0:
+        C3 = C3+1
+    elif C3 == 1:
+        C3 = C3-1
+
+
+def f_C4():
+    if C4 == 0:
+        C4 = C4+1
+    elif C4 == 1:
+        C4 = C4-1
+
+
+def f_C5():
+    if C5 == 0:
+        C5 = C5+1
+    elif C5 == 1:
+        C5 = C5-1
+
+
+def f_C6():
+    if C6 == 0:
+        C6 = C6+1
+    elif C6 == 1:
+        C6 = C6-1
+
+
+def f_C7():
+    if C7 == 0:
+        C7 = C7+1
+    elif C7 == 1:
+        C7 = C7-1
+
+
+def f_C8():
+    if C8 == 0:
+        C8 = C8+1
+    elif C8 == 1:
+        C8 = C8-1
+
+
+def f_C9():
+    if C9 == 0:
+        C9 = C9+1
+    elif C9 == 1:
+        C9 = C9-1
+
+
+def f_C10():
+    if C10 == 0:
+        C10 = C10+1
+    elif C10 == 1:
+        C10 = C10-1
+
+
+def f_C11():
+    if C11 == 0:
+        C11 = C11+1
+    elif C11 == 1:
+        C11 = C11-1
+
+
+def f_C12():
+    if C12 == 0:
+        C12 = C12+1
+    elif C12 == 1:
+        C12 = C12-1
+
+
+def f_C13():
+    if C13 == 0:
+        C13 = C13+1
+    elif C13 == 1:
+        C13 = C13-1
+
+
+def f_C14():
+    if C14 == 0:
+        C14 = C14+1
+    elif C14 == 1:
+        C14 = C14-1
+
+
+def f_C15():
+    if C15 == 0:
+        C15 = C15+1
+    elif C15 == 1:
+        C15 = C15-1
+
+
+def f_C16():
+    if C16 == 0:
+        C16 = C16+1
+    elif C16 == 1:
+        C16 = C16-1
+
